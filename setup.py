d8083a84634285773cef365f38e79b23074b5bbd
elie <elie>
2002-10-02 08:22:57 +0000

    Version turned 3.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="pysnmp",
-      version="3.0.0",
+      version="3.0.1",
       description="Python SNMP Toolkit",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
