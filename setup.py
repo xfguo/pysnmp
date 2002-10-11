641352fa743bad74813b60ce5ec7d5655ba32f09
elie <elie>
2002-10-11 13:53:39 +0000

    bugfix: all sub-packages must be explicitly listed in "packages" list

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 from distutils.core import setup
 
 setup(name="pysnmp",
-      version="3.1.0",
+      version="3.1.1",
       description="Python SNMP Toolkit",
       author="Ilya Etingof",
       author_email="ilya@glas.net ",
       url="http://sourceforge.net/projects/pysnmp/",
-      package_dir = {'': 'pysnmp'},
+      packages = [ 'pysnmp',
+                   'pysnmp.asn1',
+                   'pysnmp.asn1.ber',
+                   'pysnmp.proto',
+                   'pysnmp.mapping',
+                   'pysnmp.mapping.udp',
+                   'pysnmp.compat',
+                   'pysnmp.compat.pysnmp1x',
+                   'pysnmp.compat.pysnmp2x',
+                   'pysnmp.compat.snmpy' ],
       license="BSD"
       )
