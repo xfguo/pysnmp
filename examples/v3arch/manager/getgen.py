eae4bc88a08b9b487d2620cb47dedb9f238d7e8e
elie <elie>
2005-06-14 08:58:56 +0000

    converted to the latest SNMPv3 model API

--- examples/v3arch/manager/getgen.py
+++ examples/v3arch/manager/getgen.py
@@ -1,69 +1,53 @@
-"""Command Generator Application (GET)"""
-from pysnmp.proto.rfc3412 import MsgAndPduDispatcher, AbstractApplication
-from pysnmp.proto import omni
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asynsock.dgram import udp
+from pysnmp.entity.rfc3413 import cmdgen, error
 
-# PDU version to use
-versionId = omni.protoVersionId1
-ver = omni.protoVersions[versionId]
+snmpEngine = engine.SnmpEngine()
 
-class ManagerApplication(AbstractApplication):
-    __pendingReqs = {}
-    def sendReq(self, msgAndPduDsp, **kwargs):
-        sendPduHandle = apply(msgAndPduDsp.sendPdu, (), kwargs)
-        # Queue request information
-        self.__pendingReqs[sendPduHandle] = kwargs['PDU']
-            
-    def processResponsePdu(self, msgAndPduDsp, **kwargs):
-        # Take pending req off the queue        
-        reqPdu = self.__pendingReqs.get(kwargs['sendPduHandle'])
-        del self.__pendingReqs[kwargs['sendPduHandle']]
-
-        # Check for SNMP engine-level errors
-        if kwargs.has_key('statusInformation'):
-            raise str(kwargs['statusInformation'])
-        
-        rspPdu = kwargs['PDU']
-
-        # Check for PDU-level errors
-        errorStatus = rspPdu.omniGetErrorStatus()
-        if errorStatus:
-            raise str(errorStatus)
-
-        # Report response values
-        for varBind in rspPdu.omniGetVarBindList():
-            oid, val = varBind.omniGetOidVal()
-            print oid, val
-        msgAndPduDsp.transportDispatcher.doDispatchFlag = 0
-
-msgAndPduDsp = MsgAndPduDispatcher()
-
-# UDP is default transport, initialize client mode
-msgAndPduDsp.transportDispatcher.getTransport('udp').openClientMode()
-
-# Configure target SNMP agent at LCD
-( snmpCommunityEntry, )  \
-  =  msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
-    'SNMP-COMMUNITY-MIB', 'snmpCommunityEntry'
-    )
-msgAndPduDsp.mibInstrumController.writeVars(
-    (snmpCommunityEntry.getInstNameByIndex(2, 'myAgentIdx'), 'public'),
-    (snmpCommunityEntry.getInstNameByIndex(3, 'myAgentIdx'), 'myAgent')
+# Setup transport endpoint
+config.addSocketTransport(
+    snmpEngine,
+    udp.domainName,
+    udp.UdpSocketTransport().openClientMode()
     )
 
-pdu = ver.GetRequestPdu()
-pdu.omniSetVarBindList(((1,3,6,1,2,1,1,1), ver.Null()))
+# v1/2 setup
+# addV1System(snmpEngine, 'public')
 
-app = ManagerApplication()
-app.sendReq(
-    msgAndPduDsp,
-    transportDomain='udp', transportAddress=('127.0.0.1', 1161),
-    messageProcessingModel=versionId,
-    pduVersion=versionId,
-    securityName='myAgent',
-    PDU=pdu,
-    expectResponse=app
+# v3 setup
+config.addV3User(snmpEngine, 'test-user', 'authkey1', 'md5', 'privkey1', 'des')
+
+# Transport params
+config.addTargetParams(snmpEngine, 'myParams', 'test-user', 'authPriv')
+
+# Transport addresses
+config.addTargetAddr(
+    snmpEngine, 'myRouter', config.snmpUDPDomain,
+    ('127.0.0.1', 161), 'myParams'
     )
 
-msgAndPduDsp.transportDispatcher.runDispatcher()
+# Transport
+config.addSocketTransport(
+    snmpEngine,
+    config.snmpUDPDomain,
+    udp.UdpSocketTransport().openClientMode()
+    )
 
+def cbFun(sendRequestHandle, errorIndication, errorStatus, errorIndex,
+          varBinds, cbCtx):
+    raise error.ApplicationReturn(
+        errorIndication=errorIndication,
+        errorStatus=errorStatus,
+        errorIndex=errorIndex,
+        varBinds=varBinds
+        )
     
+cmdgen.SnmpGet().sendReq(
+    snmpEngine, 'myRouter', (((1,3,6,1,2,1,1,1,0), None),), cbFun
+    )
+
+try:
+    snmpEngine.transportDispatcher.runDispatcher()
+except error.ApplicationReturn, applicationReturn:
+    for oid, val in applicationReturn['varBinds']:
+        print '%s = %s' % (oid, val)    
--- examples/v3arch/manager/nextgen.py
+++ examples/v3arch/manager/nextgen.py
@@ -1,109 +1,52 @@
-"""Command Generator Application (GETNEXT)"""
-from pysnmp.proto.rfc3412 import MsgAndPduDispatcher, AbstractApplication
-from pysnmp.proto import omni
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asynsock.dgram import udp
+from pysnmp.entity.rfc3413 import cmdgen, error
 
-# PDU version to use
-versionId = omni.protoVersionId1
-ver = omni.protoVersions[versionId]
+snmpEngine = engine.SnmpEngine()
 
-# SNMP table header
-headVars = [ ver.ObjectName((1,3,6,1,2,1,1)) ]
-
-class ManagerApplication(AbstractApplication):
-    __pendingReqs = {}
-    def sendReq(self, msgAndPduDsp, **kwargs):
-        sendPduHandle = apply(msgAndPduDsp.sendPdu, (), kwargs)
-        # Queue request information
-        self.__pendingReqs[sendPduHandle] = (
-            kwargs['transportDomain'],
-            kwargs['transportAddress'],
-            kwargs['PDU']
-            )
-            
-    def processResponsePdu(self, msgAndPduDsp, **kwargs):
-        # Take pending req off the queue
-        transportDomain, transportAddress, reqPdu = self.__pendingReqs.get(
-            kwargs['sendPduHandle']
-            )
-        del self.__pendingReqs[kwargs['sendPduHandle']]
-
-        # Check for SNMP engine-level errors        
-        if kwargs.has_key('statusInformation'):
-            raise str(kwargs['statusInformation'])
-        
-        rspPdu = kwargs['PDU']
-
-        # Check for PDU-level error
-        errorStatus = rspPdu.omniGetErrorStatus()
-        if 0 != errorStatus != 2:
-            raise str(errorStatus)
-
-        # Build SNMP table from response
-        tableIndices = apply(
-            reqPdu.omniGetTableIndices, [rspPdu] + headVars
-            )
-
-        # Report SNMP table
-        varBindList = rspPdu.omniGetVarBindList()
-        for rowIndices in tableIndices:
-            for cellIdx in filter(lambda x: x!=-1, rowIndices):
-                print varBindList[cellIdx].omniGetOidVal()
-
-        # Remove completed SNMP table columns
-        map(lambda idx, headVars=headVars: headVars.__delitem__(idx), \
-            filter(lambda x: x==-1, tableIndices[-1]))
-        if not headVars:
-            msgAndPduDsp.transportDispatcher.doDispatchFlag = 0
-            return
-
-        # Generate request PDU for next row
-        lastRow = map(lambda cellIdx, varBindList=varBindList:
-                      varBindList[cellIdx].omniGetOidVal(),
-                      filter(lambda x: x!=-1, tableIndices[-1]))
-        apply(reqPdu.omniSetVarBindList,
-              map(lambda (x, y): (x.get(), None), lastRow))
-        
-        reqPdu.omniGetRequestId().inc(1)
-
-        # Submit next req to msgAndPduDsp
-        self.sendReq(
-            msgAndPduDsp,
-            transportDomain=transportDomain,
-            transportAddress=transportAddress,
-            messageProcessingModel=kwargs['messageProcessingModel'],
-            pduVersion=kwargs['pduVersion'],
-            securityName=kwargs['securityName'],            
-            PDU=reqPdu,
-            expectResponse=self
-            )                     
-                     
-msgAndPduDsp = MsgAndPduDispatcher()
-
-# UDP is default transport, initialize client mode
-msgAndPduDsp.transportDispatcher.getTransport('udp').openClientMode()
-
-# Configure target SNMP agent at LCD
-( snmpCommunityEntry, )  \
-  =  msgAndPduDsp.mibInstrumController.mibBuilder.importSymbols(
-    'SNMP-COMMUNITY-MIB', 'snmpCommunityEntry'
-    )
-msgAndPduDsp.mibInstrumController.writeVars(
-    (snmpCommunityEntry.getInstNameByIndex(2, 'myAgentIdx'), 'public'),
-    (snmpCommunityEntry.getInstNameByIndex(3, 'myAgentIdx'), 'myAgent')
+# Setup transport endpoint
+config.addSocketTransport(
+    snmpEngine,
+    udp.domainName,
+    udp.UdpSocketTransport().openClientMode()
     )
 
-pdu = ver.GetNextRequestPdu()
-pdu.omniSetVarBindList((headVars[0].clone(), ver.Null()))
+# v1/2 setup
+# addV1System(snmpEngine, 'public')
 
-app = ManagerApplication()
-app.sendReq(
-    msgAndPduDsp,
-    transportDomain='udp', transportAddress=('127.0.0.1', 1161),
-    messageProcessingModel=versionId,
-    pduVersion=versionId,
-    securityName='myAgent',
-    PDU=pdu,
-    expectResponse=app
+# v3 setup
+config.addV3User(snmpEngine, 'test-user', 'authkey1', 'md5', 'privkey1', 'des')
+
+# Transport params
+config.addTargetParams(snmpEngine, 'myParams', 'test-user', 'authPriv')
+
+# Transport addresses
+config.addTargetAddr(
+    snmpEngine, 'myRouter', config.snmpUDPDomain,
+    ('127.0.0.1', 161), 'myParams'    
     )
 
-msgAndPduDsp.transportDispatcher.runDispatcher()
+# Transport
+config.addSocketTransport(
+    snmpEngine,
+    config.snmpUDPDomain,
+    udp.UdpSocketTransport().openClientMode()
+    )
+
+def cbFun(sendRequestHandle, errorIndication, errorStatus, errorIndex,
+          varBinds, cbCtx):
+    if errorIndication or errorStatus:
+        raise error.ApplicationReturn(
+            errorIndication=errorIndication,
+            errorStatus=errorStatus
+            )
+    for oid, val in varBinds: print '%s = %s' % (oid, val)
+
+cmdgen.SnmpWalk().sendReq(
+    snmpEngine, 'myRouter', (((1,3,6,1,2,1), None),), cbFun
+    )
+
+try:
+    snmpEngine.transportDispatcher.runDispatcher()
+except error.ApplicationReturn, applicationReturn:
+    print applicationReturn
