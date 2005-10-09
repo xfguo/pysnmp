9b254db5e5cb2774eb32078b2a58b92e41c572f2
elie <elie>
2005-10-09 18:09:52 +0000

    adjusted to latest NotificationOriginator interface

--- examples/v3arch/agent/ntforg.py
+++ examples/v3arch/agent/ntforg.py
@@ -1,6 +1,7 @@
 from pysnmp.entity import engine, config
 from pysnmp.carrier.asynsock.dgram import udp
 from pysnmp.entity.rfc3413 import ntforg, context
+from pysnmp.proto.api import v2c
 
 snmpEngine = engine.SnmpEngine()
 
@@ -15,13 +16,13 @@ config.addV3User(
     )
 
 # Transport params
-config.addTargetParams(snmpEngine, 'myParams', 'test-user', 'authPriv')
-#config.addTargetParams(snmpEngine, 'myParams', 'test-agent', 'noAuthNoPriv', 1)
+#config.addTargetParams(snmpEngine, 'myParams', 'test-user', 'authPriv')
+config.addTargetParams(snmpEngine, 'myParams', 'test-agent', 'noAuthNoPriv', 0)
 
 # Transport addresses
 config.addTargetAddr(
     snmpEngine, 'myNMS', config.snmpUDPDomain,
-    ('127.0.0.1', 162), 'myParams', tagList='myManagementStations'
+    ('127.0.0.1', 1162), 'myParams', tagList='myManagementStations'
     )
 
 # Notification targets
@@ -36,19 +37,18 @@ config.addSocketTransport(
     udp.UdpSocketTransport().openClientMode()
     )
 
-def cbFun(sendRequestHandle, errorIndication, errorStatus, errorIndex,
-          varBinds, cbCtx):
-    return
-
 # Agent-side VACM setup
 config.addContext(snmpEngine, '')
+config.addTrapUser(snmpEngine, 1, 'test-agent', 'noAuthNoPriv', (1,3,6)) # v1
+config.addTrapUser(snmpEngine, 2, 'test-agent', 'noAuthNoPriv', (1,3,6)) # v2c
 config.addTrapUser(snmpEngine, 3, 'test-user', 'authPriv', (1,3,6)) # v3
 
 # SNMP context
 snmpContext = context.SnmpContext(snmpEngine)
     
 ntforg.NotificationOriginator(snmpContext).sendNotification(
-    snmpEngine, 'myNotifyName', ('SNMPv2-MIB', 'coldStart'),(), '', cbFun
+    snmpEngine, 'myNotifyName', ('SNMPv2-MIB', 'coldStart'),
+    (((1,3,6,1,2,1,1,5), v2c.Integer(1)),), ''
     )
 
 snmpEngine.transportDispatcher.runDispatcher()
