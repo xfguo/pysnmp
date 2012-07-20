5e3e500ec45ebb405dbcac96af15a41c91854aa6
elie <elie>
2012-07-20 15:36:05 +0000

    fix to Notification Originator to make it communicating a single sendPduHandle to an application when multiple INFORMs are triggered and processed by a single call by way of transport tagging feature

--- examples/v3arch/agent/ntforg.py
+++ examples/v3arch/agent/ntforg.py
@@ -3,10 +3,11 @@ from pysnmp.entity import engine, config
 from pysnmp.carrier.asynsock.dgram import udp, udp6, unix
 from pysnmp.entity.rfc3413 import ntforg, context
 from pysnmp.proto.api import v2c
-#from pysnmp import debug
+from pysnmp import debug
 
 ## Optional debugging ('all' enables full debugging)
 #debug.setLogger(debug.Debug('io', 'dsp', 'msgproc', 'secmod', 'app'))
+#debug.setLogger(debug.Debug('app'))
 
 # Create SNMP engine instance
 snmpEngine = engine.SnmpEngine()
@@ -19,20 +20,20 @@ snmpEngine = engine.SnmpEngine()
 config.addV1System(snmpEngine, 'my-area', 'public')
 
 ## Specify security settings per SecurityName (SNMPv1 - 0, SNMPv2c - 1)
-#config.addTargetParams(snmpEngine, 'my-creds', 'my-area', 'noAuthNoPriv', 0)
-config.addTargetParams(snmpEngine, 'my-creds', 'my-area', 'noAuthNoPriv', 1)
+#config.addTargetParams(snmpEngine, 'my-creds-1', 'my-area', 'noAuthNoPriv', 0)
+config.addTargetParams(snmpEngine, 'my-creds-1', 'my-area', 'noAuthNoPriv', 1)
 
 #
 # SNMPv3/USM setup (choose any one if you use SNMPv3/USM)
 #
 
 # user: usr-md5-des, auth: MD5, priv DES
-#config.addV3User(
-#    snmpEngine, 'usr-md5-des',
-#    config.usmHMACMD5AuthProtocol, 'authkey1',
-#    config.usmDESPrivProtocol, 'privkey1'
-#)
-#config.addTargetParams(snmpEngine, 'my-creds', 'usr-md5-des', 'authPriv')
+config.addV3User(
+    snmpEngine, 'usr-md5-des',
+    config.usmHMACMD5AuthProtocol, 'authkey1',
+    config.usmDESPrivProtocol, 'privkey1'
+)
+config.addTargetParams(snmpEngine, 'my-creds-3', 'usr-md5-des', 'authPriv')
 
 ## user: usr-none-none, auth: NONE, priv NONE
 #config.addV3User(
@@ -94,7 +95,7 @@ config.addSocketTransport(
 config.addTargetAddr(
     snmpEngine, 'my-nms-1',
     udp.domainName, ('127.0.0.1', 162),
-    'my-creds',
+    'my-creds-1',
     tagList='all-my-managers'
 )
 
@@ -107,7 +108,7 @@ config.addSocketTransport(
 config.addTargetAddr(
     snmpEngine, 'my-nms-2',
     udp6.domainName, ('::1', 162),
-    'my-creds',
+    'my-creds-3',
     tagList='all-my-managers'
 )
 
@@ -157,10 +158,9 @@ snmpContext = context.SnmpContext(snmpEngine)
 
 # # Error/confirmation reciever
 def cbFun(sendRequestHandle, errorIndication, cbCtx):
-    if errorIndication:
-        print(errorIndication)
-        
-ntforg.NotificationOriginator(snmpContext).sendNotification(
+    print('Notification %s, status - %s' % (sendRequestHandle, errorIndication and errorIndication or 'delivered'))
+ 
+sendRequestHandle = ntforg.NotificationOriginator(snmpContext).sendNotification(
     snmpEngine,
     # Notification targets
     'my-notification',
@@ -171,6 +171,8 @@ ntforg.NotificationOriginator(snmpContext).sendNotification(
       ((1,3,6,1,2,1,1,5), v2c.OctetString('Notificator Example')) ),
     cbFun
 )
+
+print('Notification %s scheduled to be sent' % sendRequestHandle)
 
 # Run I/O dispatcher which would send pending message and process response
 snmpEngine.transportDispatcher.runDispatcher()
